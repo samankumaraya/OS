581e94a5fa44de19326f0d9196853b45234b1414
Saman Kumara <samankumararajapaksha660@gmail.com>
2022-11-04 02:11:29 +0530

    implement_with_c

--- loader.s
+++ loader.s
@@ -1,17 +1,30 @@
-global loader                   ; the entry symbol for ELF
+global loader
 
-    MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                    ; (magic number + checksum + flags should equal 0)
+extern kmain
 
-    section .text:                  ; start of the text (code) section
-    align 4                         ; the code must be 4 byte aligned
-        dd MAGIC_NUMBER             ; write the magic number to the machine code,
-        dd FLAGS                    ; the flags,
-        dd CHECKSUM                 ; and the checksum
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS 	     equ 0x0
+CHECKSUM     equ -MAGIC_NUMBER
 
-    loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
-    .loop:
-        jmp .loop                   ; loop forever
+section .text:
+align 4				
+	dd MAGIC_NUMBER
+	dd FLAGS
+	dd CHECKSUM
+
+KERNEL_STACK_SIZE equ 4096
+
+section .bss
+align 4
+kernel_stack:
+	resb KERNEL_STACK_SIZE
+
+loader:
+	mov esp, kernel_stack + KERNEL_STACK_SIZE
+	push dword 3
+	push dword 2
+	push dword 1
+	call kmain
+
+.loop:
+	jmp .loop
